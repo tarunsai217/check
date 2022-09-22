5d6eece91738da40511694aa2d3530f1a098f23d
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-22 10:20:02 +0530

    feat- portfolio added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import TopNav from './Components/TopNav'
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Experience from './Components/Experience';
 function App() {
   return (
     <div className="app">
       <h1>Tarun Sai</h1>
       <TopNav/>
+      <About />
+      <Skills/>
+      <Projects/>
+      <Experience/>
     </div>
   );
 }
