6dc90031bf7c347830303569c635a81bf46a9514
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-22 10:24:23 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import TopNav from './Components/TopNav'
 import About from './Components/About';
-import Skills from './Components/Skills';
-import Projects from './Components/Projects';
-import Experience from './Components/Experience';
+// import Skills from './Components/Skills';
+// import Projects from './Components/Projects';
+// import Experience from './Components/Experience';
 function App() {
   return (
     <div className="app">
       <h1>Tarun Sai</h1>
       <TopNav/>
       <About />
-      <Skills/>
+      {/* <Skills/>
       <Projects/>
-      <Experience/>
+      <Experience/> */}
     </div>
   );
 }
