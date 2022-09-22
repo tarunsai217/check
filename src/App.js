efe6953e01ff61d672e5192d54cc2ca7ff46f156
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-22 10:53:32 +0530

    projects added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TopNav from './Components/TopNav'
 import About from './Components/About';
 import Skills from './Components/Skills';
-// import Projects from './Components/Projects';
+import Projects from './Components/Projects';
 import Experience from './Components/Experience';
 // import Footer from './Components/Footer'
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <TopNav/>
       <About />
       <Skills/>
-      {/* <Projects/> */}
+      <Projects/>
       <Experience/>
       {/* <Footer/> */}
     </div>
