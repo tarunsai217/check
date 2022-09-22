e733027921e31dc74ae0e8b91383875bc2ffb183
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-22 10:14:56 +0530

    feat-topnav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import TopNav from './Components/TopNav'
 function App() {
   return (
     <div className="app">
       <h1>Tarun Sai</h1>
+      <TopNav/>
     </div>
   );
 }
